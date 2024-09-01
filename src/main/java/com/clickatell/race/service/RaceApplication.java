aff9b35eefbcbae6db57d15487e95416f6ea8e34
Emmanuel-Irabor <emmanuelirabor@palmpay-inc.com>
2024-09-01 18:41:57 +0100

    feat(main): initialize spring boot RaceApplication

--- src/main/java/com/clickatell/race/service/RaceApplication.java
+++ src/main/java/com/clickatell/race/service/RaceApplication.java
@@ -1,8 +1,22 @@
 package com.clickatell.race.service;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Emmanuel-Irabor
  * @since 01/09/2024
  */
+
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.clickatell"
+        }
+)
 public class RaceApplication {
+        public static void main(String[] args) {
+                SpringApplication.run(RaceApplication.class,args);
+                System.out.println("RaceApplication Start success!");
+        }
 }
