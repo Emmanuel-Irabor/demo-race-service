5f72007d8a2b8c5287b0a3bdbb8038e26101023a
Emmanuel-Irabor <emmanuelirabor@palmpay-inc.com>
2024-09-03 00:14:52 +0100

    feat(main): create RiderRequest and RiderResponse classes

---
+++ src/main/java/com/clickatell/race/api/request/RiderRequest.java
@@ -0,0 +1,13 @@
+package com.clickatell.race.api.request;
+
+import lombok.Data;
+
+/**
+ * @author Emmanuel-Irabor
+ * @since 03/09/2024
+ */
+@Data
+public class RiderRequest {
+
+    private String riderName;
+}
